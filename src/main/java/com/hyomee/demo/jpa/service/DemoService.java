1e570ba3a86205e7005e23714fcc220f7231867e
hyosang <hyomee@naver.com>
2023-08-19 17:30:07 +0900

    오류 메세지

--- src/main/java/com/hyomee/demo/jpa/service/DemoService.java
+++ src/main/java/com/hyomee/demo/jpa/service/DemoService.java
@@ -1,6 +1,5 @@
 package com.hyomee.demo.jpa.service;
 
-import com.hyomee.core.customException.BizException;
 import com.hyomee.demo.jpa.dto.DemoDTO;
 import com.hyomee.demo.jpa.repository.DemoMapper;
 import com.hyomee.demo.jpa.entity.DemoEntity;
@@ -21,30 +20,14 @@ public class DemoService {
   public List<DemoDTO> retrieveDemo() {
     List<DemoEntity> demoEntities = demoRepository.findAll();
     log.debug(demoEntities.toString());
-    return DemoMapper.INSTANCE.toDemoEntirys(demoEntities);
+    return DemoMapper.INSTANCE.toDemoDTOs(demoEntities);
   }
 
-  public List<DemoDTO> retrieveDemoException() {
-    List<DemoEntity> demoEntities = demoRepository.findAll();
-    if (true) throw new BizException("ERR_0002");
-    return DemoMapper.INSTANCE.toDemoEntirys(demoEntities);
+
+  public DemoDTO saveDemo(DemoDTO demoDTO ) {
+    DemoEntity demoEntity = DemoMapper.INSTANCE.toDemoEntitys(demoDTO);
+    DemoEntity demoEntitySave = demoRepository.save(demoEntity);
+    return DemoMapper.INSTANCE.toDemoDTO(demoEntitySave);
   }
 
-  public List<DemoDTO> retrieveDemoNoCodeException() {
-    List<DemoEntity> demoEntities = demoRepository.findAll();
-    if (true) throw new BizException("헉 이건 뭐지 글자 그대로 오류 발생 .....");
-    return DemoMapper.INSTANCE.toDemoEntirys(demoEntities);
-  }
-
-  public List<DemoDTO> retrieveDemoArrayException() {
-    List<DemoEntity> demoEntities = demoRepository.findAll();
-    if (true) throw new BizException("ERR_0003", "오류1", "오류2");
-    return DemoMapper.INSTANCE.toDemoEntirys(demoEntities);
-  }
-
-  public List<DemoDTO> retrieveDemoNoCodeFormatException() {
-    List<DemoEntity> demoEntities = demoRepository.findAll();
-    if (true) throw new BizException("헉 이건 뭐지 글자 {0} 그대로 오류 발생 {1}.....", "포맷터1", "포맷터2");
-    return DemoMapper.INSTANCE.toDemoEntirys(demoEntities);
-  }
 }
