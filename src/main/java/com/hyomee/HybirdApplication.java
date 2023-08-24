4e677191e6ea880f8ebffa51112f78f04f88e478
hyosang <hyomee@naver.com>
2023-08-25 01:10:03 +0900

    jpa 기본 페이징

--- src/main/java/com/hyomee/HybirdApplication.java
+++ src/main/java/com/hyomee/HybirdApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling   // 스케줄링 기능활성화
 @EnableJpaRepositories(basePackages = { "com.hyomee.core.jpa", "com.hyomee.demo.jpa", "com.hyomee.work.workService" })
 @EnableElasticsearchRepositories(basePackages = { "com.hyomee.demo.es", "com.hyomee.work.workEcService" })
-@ComponentScan(basePackages = {"com.hyomee"})
 @SpringBootApplication
 public class HybirdApplication {  public static void main(String[] args) {
     SpringApplication.run(HybirdApplication.class, args);
